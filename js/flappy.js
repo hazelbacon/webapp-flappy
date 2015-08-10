516229084ee3efebabcee31538214668140ced08
V3491 (Conference ID) <v3491@cam.ac.uk>
2015-08-10 17:02:55 +0100

    monday

--- js/flappy.js
+++ js/flappy.js
@@ -12,92 +12,94 @@ var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
 /*
  * Loads all resources for the game and gives them names.
  */
-function preload() {game.load.image("InitialF", "../assets/fish.PNG");
-    game.load.image("additionalImg2", "../assets/b.png");
-    game.load.image("Player1", "../assets/player.png");
-    game.load.image("pipe","../assets/pipe_blue.png");
+function preload(){
+    game.load.image("additionalsecondImg", "../assets/paw.png");
+    game.load.image("playersecondImg", "../assets/kittenplayer.png");
+    game.load.image("pipe","../assets/pipe_purple.png");
+
 
 }
 
+/*
+ * Initialises the game. This function is only called once.
+ */
 var score;
 var labelScore;
 var player;
 var pipes = [];
 
-function create() {game.stage.setBackgroundColor('#3385D6');
-    // set the background colour of the scene
-    game.add.text(90,100,"Flying Fish!",
-    {font: "100px Ebrima", fill: "#D6D6FF"});
 
-    var background = game.add.sprite(600, 160, "InitialF");
-    background.width = 200;
-    background.height = 75;
-    var background = game.add.sprite(580,110, "additionalImg2");
-    background.width = 60;
-    background.height = 50;
-    var background = game.add.sprite(650,80, "additionalImg2");
-    background.width = 60;
-    background.height = 50;
-    var background = game.add.sprite(580,40, "additionalImg2");
-    background.width = 70;
-    background.height = 60;
-    var background = game.add.sprite(610,165, "additionalImg2");
-    background.width = 40;
-    background.height = 30;
+
+
+function create() {game.stage.setBackgroundColor ("#F0B2F0");
+// set the background colour of the scene
+    game.add.text(310,10, "Welcome",
+        {font: "40px Verdana", fill: "#9900CC"});
+
+    player = game.add.sprite(350, 200, "playersecondImg");
+    player.width = 40;
+    player.height = 40;
+
+    game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+        .onDown.add(moveRight);
+    game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+        .onDown.add(moveLeft);
+    game.input.keyboard.addKey(Phaser.Keyboard.UP)
+        .onDown.add(moveUp);
+    game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
+        .onDown.add(moveDown);
+
+    score = 0;
+    labelScore = game.add.text(20,20, "0");
+
     game.input
         .onDown
         .add(clickHandler);
 
-    score = 0;
-labelScore = game.add.text(20,20,"0");
-
-
-    player = game.add.sprite(350,100, "Player1" );
-    player.width = 60;
-    player.height = 30;
-
-
-    game.input.keyboard.addKey(Phaser.Keyboard.UP)
-        .onDown.add(moveup);
-    game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
-        .onDown.add(movedown);
-
     generatePipe();
 
-game.physics.startSystem(Phaser.Physics.ARCADE)
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.enable(player);
     player.body.velocity.x = 0;
-    player.body.velocity.y = -100;
-    player.body.gravity.y = 400;
-
+    player.body.velocity.y = -150;
+    player.body.gravity.y = 380;
 
     game.input.keyboard
         .addKey(Phaser.Keyboard.SPACEBAR)
         .onDown.add(playerJump);
 
-    pipeInterval = 2;
+    pipeInterval = 1.75;
     game.time.events
         .loop(pipeInterval * Phaser.Timer.SECOND,
         generatePipe);
 
 }
 
-function clickHandler(event) {
-    var background = game.add.sprite(event.x, event.y, "additionalImg2");
-    background.width = 50;
-    background.height = 40;
+
+function clickHandler(event){
+    var background = game.add.sprite(event.x,event.y,"additionalsecondImg");
+    background.width = 80;
+    background.height = 80;
 }
 /*
  * This function updates the scene. It is called for every new frame.
  */
-function update() {
-    for (var index = 0; index < pipes.length; index++) {
-    game.physics.arcade
-        .overlap(player,
+function update(){
+    for(var index=0; index<pipes.length; index++){
+        game.physics.arcade
+            .overlap(player,
         pipes[index],
-        gameOver)
+        game0ver);
+    }
 }
+
+
+
+function game0ver() {
+    location.reload();
 }
+
+
 
 function changeScore(){
     score = score + 1;
@@ -105,37 +107,43 @@ function changeScore(){
 
 }
 
+function moveRight(){
+    player.x += 30;
+}
 
-function moveup(){
-    player.y -=50
+function moveLeft(){
+    player.x -= 30;
 }
-function movedown(){
-    player.y +=50
+
+function moveUp(){
+ player.y -= 30;
 }
-function generatePipe(){
+
+function moveDown() {
+    player.y += 30;
+}
+
+
+
+function generatePipe() {
     //calculate a random position for the gap
     var gap = game.rnd.integerInRange(1, 5);
-    //generate the pipes, except where the gap should be
-    for (var count=0; count<8; count++) {
-        if(count != gap && count != gap+1) {
-            addPipeBlock(750, count*50);
+    // generate the pipes, except where the gap should be
+    for (var count = 0; count < 8; count++) {
+        if (count != gap && count != gap + 1) {
+            addPipeBlock(750, count * 50);
         }
-    }
-    changeScore();
+    }changeScore();
 }
+
 
 function addPipeBlock(x, y) {
-    var pipeBlock = game.add.sprite(x, y, "pipe");
+    var pipeBlock = game.add.sprite(x,y,"pipe");
     pipes.push(pipeBlock);
-    game.physics.arcade.enable(pipeBlock)
-    pipeBlock.body.velocity.x = -200
+    game.physics.arcade.enable(pipeBlock);
+    pipeBlock.body.velocity.x = -200;
 }
 
-function playerJump(){
-    player.body.velocity.y = -200;
-}
-
-function gameOver(){
-    location.reload();
-
+function playerJump (){
+    player.body.velocity.y = -170
 }
