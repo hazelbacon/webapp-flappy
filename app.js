402c38e2c4d66906b955be8a830d711bbef23a64
V5794 (Course ID) <v5794@cam.ac.uk>
2015-08-14 14:18:48 +0100

    flappy

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+var csv = require('ya-csv');
+
 
 var app = express();
 app.use(express.static(path.join(__dirname, "")));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(request, response){
+app.get('/', function(request, response){
     response.sendFile(path.join(__dirname, "pages/index.html"));
 });
+app.get("/score", function(request, response) {
+    var reader = csv.createCsvFileReader("scores.csv");
+    reader.setColumnNames(['name', 'score']);
+    var scores = [];
+    reader.addListener('data', function(data) {
+        scores.push(data);
+    });
+    reader.addListener('end', function(){
+        response.send(scores);
+    })
+});
+
 
 app.post('/score', function(request, response){
-    
+    var name = request.body.fullName;
+    var score = request.body.score;
+    var database = csv.createCsvFileWriter("scores.csv", {"flags": "a"});
+    var data = [name, score];
+    database.writeRecord(data);
+    database.writeStream.end();
+    response.send("Thanks " + name + ", your score has been recorded - click to play more http://localhost:8080/");
 });
+
 
 var server = app.listen(8080, function() {
     var host = server.address().address;
